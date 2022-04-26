4a05637cf7c1e18bc5c0745782ad3d6532ab7861
StR0vErBG <xcreaw1@abv.bg>
2022-04-26 22:05:23 +0200

    Basic form structure

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import "./form.scss";
 
 export default function Form() {
   return (
-    <form action="">
+    <form action="d-flex flex-row">
+        <div>
     <input type="name" placeholder='Full name'/>
+
+        </div>
+        <div>
+
     <input type="email" placeholder='Email address'/>
+        </div>
+    <div>
     <textarea name="textMe" id="" cols="30" rows="10" placeholder="Text"></textarea>
+    </div>
+    <button className="btn">Send</button>
 </form>
   )
 }
