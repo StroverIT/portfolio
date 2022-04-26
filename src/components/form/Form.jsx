3cfa04574098d067f40032b133378d8a462081ae
StR0vErBG <xcreaw1@abv.bg>
2022-04-26 22:02:49 +0200

    Adding form component

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Form() {
+  return (
+    <form action="">
+    <input type="name" placeholder='Full name'/>
+    <input type="email" placeholder='Email address'/>
+    <textarea name="textMe" id="" cols="30" rows="10" placeholder="Text"></textarea>
+</form>
+  )
+}
