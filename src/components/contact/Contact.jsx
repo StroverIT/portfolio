bc14f86f37c4054eb27ef7265e5d5f254ce47cfa
StR0vErBG <xcreaw1@abv.bg>
2022-04-26 22:00:19 +0200

    Basic structure in jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,9 +3,12 @@ import "./contact.scss";
 
 const Contact = () => {
     return (
-        <div className="d-flex justify-content-center align-items-center flex-column">
-            <h4 className="">Get In Touch</h4>
-            <p>
+        <section className="contact d-flex justify-content-center align-items-center flex-column">
+           <div className="pb-1">
+       <p className="title primary-color titleNext">What's next?</p>
+     </div>
+            <h4 className="title-color bigger-title getInTouch">Get In Touch</h4>
+            <p className="text col-12 col-sm-8">
                 Althrough I'm now currently looking for new opportunities, my inbox is always open. 
                 Whether you have a question or just want to say hi, I'll try my best to get back to you!
             </p>
@@ -14,7 +17,7 @@ const Contact = () => {
                 <input type="email" placeholder='Email address'/>
                 <textarea name="textMe" id="" cols="30" rows="10" placeholder="Text"></textarea>
             </form>
-        </div>
+        </section>
     );
 }
 
