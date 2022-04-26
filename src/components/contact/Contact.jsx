493dd20309845ee348837c80fdc30e1e2f8a5f52
StR0vErBG <xcreaw1@abv.bg>
2022-04-26 22:02:31 +0200

    Removingi from to make it component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,11 +12,7 @@ const Contact = () => {
                 Althrough I'm now currently looking for new opportunities, my inbox is always open. 
                 Whether you have a question or just want to say hi, I'll try my best to get back to you!
             </p>
-            <form action="">
-                <input type="name" placeholder='Full name'/>
-                <input type="email" placeholder='Email address'/>
-                <textarea name="textMe" id="" cols="30" rows="10" placeholder="Text"></textarea>
-            </form>
+          
         </section>
     );
 }
