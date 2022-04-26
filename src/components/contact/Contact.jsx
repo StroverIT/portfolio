1ec03db157a040b4be0ac8ea8005e4433d3edeb1
StR0vErBG <xcreaw1@abv.bg>
2022-04-26 22:23:41 +0200

    basic html structure

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,7 +13,10 @@ const Contact = () => {
                 Althrough I'm now currently looking for new opportunities, my inbox is always open. 
                 Whether you have a question or just want to say hi, I'll try my best to get back to you!
             </p>
+            <section className="col-12 col-sm-7">
           <Form />
+
+            </section>
         </section>
     );
 }
