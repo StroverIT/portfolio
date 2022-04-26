955becfa316e54f48f51f46b3e9b8b3025cc2604
StR0vErBG <xcreaw1@abv.bg>
2022-04-26 21:45:12 +0200

    changing to scss

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import "./contact.css";
+import "./contact.scss";
 
 const Contact = () => {
     return (
         <div>
-            <h4>Get In Touch</h4>
+            <h4 className="title">Get In Touch</h4>
             <p>
                 Althrough I'm now currently looking for new opportunities, my inbox is always open. 
                 Whether you have a question or just want to say hi, I'll try my best to get back to you!
