df4535d00ba655a1309eb54bc046b4a36d1aad3e
StR0vErBG <xcreaw1@abv.bg>
2022-04-26 22:23:50 +0200

    basic html structure

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,10 +3,9 @@ import "./form.scss";
 
 export default function Form() {
   return (
-    <form action="d-flex flex-row">
-        <div>
-    <input type="name" placeholder='Full name'/>
-
+    <form action="form d-flex flex-row form col-12">
+        <div className="d-flex">
+        <input type="name" placeholder='Full name'/>
         </div>
         <div>
 
@@ -15,7 +14,7 @@ export default function Form() {
     <div>
     <textarea name="textMe" id="" cols="30" rows="10" placeholder="Text"></textarea>
     </div>
-    <button className="btn">Send</button>
+    <button className="btn bigger-text" type="submit">Send</button>
 </form>
   )
 }
