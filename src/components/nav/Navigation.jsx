df269b2e69ddd3ed21ac8f4af8359f788d3194c8
StR0vErBG <xcreaw1@abv.bg>
2022-04-28 15:38:38 +0200

    Removing unnecessary things

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -12,15 +12,12 @@ const Navigation = () => {
         <div className="d-flex nav-links">
           <ul className="d-flex flex-column flex-md-row align-items-center">
             <li className="secondary-color">
-              {" "}
-              <a href=""><span className="primary-color">01.</span> About</a>
+              <a href="#about"><span className="primary-color">01.</span> About</a>
             </li>
             <li className="secondary-color">
-              {" "}
               <a href=""><span className="primary-color">02.</span> Work</a>
             </li>
             <li className="secondary-color">
-              {" "}
               <a href=""><span className="primary-color">03.</span> Contact</a>
             </li>
             <li className="btn">Resume</li>
