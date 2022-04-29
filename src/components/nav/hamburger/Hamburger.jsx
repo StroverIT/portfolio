b7461d0354d57cf7d42ac52f9bbbae73d2dba050
StR0vErBG <xcreaw1@abv.bg>
2022-04-29 18:13:23 +0200

    Adding hamburger

--- src/components/nav/hamburger/Hamburger.jsx
+++ src/components/nav/hamburger/Hamburger.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./hamburger.scss"
 export default function Hamburger() {
-  return (
-    <div className="header__nav__hamburger">
-    <div></div>
-    <div></div>
-    <div></div>
+  const [open, setOpen] = useState(false);
+  const hamburger= useRef(null)
+  useEffect(()=>{
+    const ul = hamburger.current.nextSibling
+      if(open){
+        hamburger.current.classList.add("open")
+        ul.classList.remove("d-none")
 
+        }else{
+          hamburger.current.classList.remove("open")
+        
+      }
+  }, [open,hamburger])
+
+  return (
+    <>
+    <div className="header__nav__hamburger d-lg-none" onClick={open ? () => setOpen(false) : () =>setOpen(true)} ref={hamburger}>
+    <div></div>
+    <div></div>
+    <div></div>
     </div>
+    </>
   )
 }
