0c8f29edf4dd4c37e930f09b931bf91b68452fa4
StR0vErBG <xcreaw1@abv.bg>
2022-04-29 15:19:38 +0200

    Adding basic structure

---
+++ src/components/nav/hamburger/Hamburger.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./hamburger.scss"
+export default function Hamburger() {
+  return (
+    <div className="header__nav__hamburger">
+    <div></div>
+    <div></div>
+    <div></div>
+
+    </div>
+  )
+}
