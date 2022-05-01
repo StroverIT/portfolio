cd0eccb3f3e7e42e4d5933450a53fc327f9d61d6
StR0vErBG <xcreaw1@abv.bg>
2022-05-01 15:39:52 +0200

    on load go to that hash

--- src/App.js
+++ src/App.js
@@ -6,8 +6,26 @@ import Contact from "./components/contact/Contact"
 import Footer from "./components/footer/Footer"
 
 import "./index.css"
+import { useEffect } from "react"
 function App() {
+  const currentUrl = new URL(window.location.href)
+  const hash = currentUrl.hash
+
+  useEffect(()=>{
+    if(hash){
+      console.log(hash);
+      const item = document.querySelector(hash)
+      console.log(item);
+      if(item){
+          
+          item.scrollIntoView({behavior: "smooth"} )
+      }
+  }
+  }, [window])
+
+
   return (
+    
     <>
       <Nav/>
     <div className="container">
