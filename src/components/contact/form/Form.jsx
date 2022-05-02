6bd8d541c01de34fadabad514bad60faab19839a
StR0vErBG <xcreaw1@abv.bg>
2022-05-02 22:24:14 +0200

    chaning convention

--- src/components/contact/form/Form.jsx
+++ src/components/contact/form/Form.jsx
@@ -32,7 +32,7 @@ export default function Form() {
     if(message){
       toastHideAll()
       console.log(message);
-      toastSuccess(message.messageEN)
+      toastSuccess(message.messageE)
     }
   }, [message]);
 
