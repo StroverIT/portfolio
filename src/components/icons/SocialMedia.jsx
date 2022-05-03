cacc5499c1ed767a1d2d7f94713a2e3e1ee6a5c6
StR0vErBG <xcreaw1@abv.bg>
2022-05-03 12:41:15 +0200

    New page on click

--- src/components/icons/SocialMedia.jsx
+++ src/components/icons/SocialMedia.jsx
@@ -5,22 +5,22 @@ export default function SocialMedia({position}) {
   return (
     <ul className={`d-flex ${position  ? position : ""}`}>
                 <li>
-                  <a href="https://github.com/StR0vErBG">
+                  <a href="https://github.com/StR0vErBG" target="_blank">
                     <BsGithub />
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.instagram.com/emil_zlatinov/">
+                  <a href="https://www.instagram.com/emil_zlatinov/" target="_blank">
                     <BsInstagram />
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.facebook.com/StR0vErBG/">
+                  <a href="https://www.facebook.com/StR0vErBG/" target="_blank">
                     <BsFacebook />
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.youtube.com/channel/UCVD7JSRRl3bYgrJXqWfJ0aA">
+                  <a href="https://www.youtube.com/channel/UCVD7JSRRl3bYgrJXqWfJ0aA" target="_blank">
                     <BsYoutube />
                   </a>
                 </li>
