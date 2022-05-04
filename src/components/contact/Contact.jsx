fb9fee5804fbc6cf3815d0ca6c401a93cb286321
StR0vErBG <xcreaw1@abv.bg>
2022-05-04 16:11:04 +0200

    fixing spacing

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
        <p className="title primary-color titleNext">What's next?</p>
      </div>
             <h4 className="title-color bigger-title getInTouch">Get In Touch</h4>
-            <p className="text col-12 col-sm-8">
+            <p className="text col-12 col-sm-8 mt-1">
                 Althrough I'm now currently looking for new opportunities, my inbox is always open. 
                 Whether you have a question or just want to say hi, I'll try my best to get back to you!
             </p>
