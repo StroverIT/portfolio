7894a00854afd27b630e383b4d31ca08e2eb8851
StR0vErBG <xcreaw1@abv.bg>
2022-05-05 12:53:25 +0200

    adding cv with bugs

---
+++ src/components/resume/CV.jsx
@@ -0,0 +1,170 @@
+import React from 'react'
+import "./cv.scss"
+export default function CV() {
+  return (
+	<div>
+		<table cellSpacing={0} cellPadding={0} border={0} class="cvTable" align="center">
+							<tr>
+						<td valign="top" style="padding-right:20px;padding-left:10px;padding-top:4px;" width={10%}><img src="me.jpg" border={0}></td>
+						<td valign="top">
+				<table cellspacing={0 }cellpadding={0} border={0 }width={100%}>
+					<tr>
+						<td><span style="font-size:21px;font-weight:bold;" >Емил Руменов Златинов</span></td>
+					</tr>
+					<tr>
+						<td align=right valign=top style="padding-left:100px;">
+							<table cellspacing=0 cellpadding=0 border=0>
+								<tr>
+									<td class="rowTitleView" valign="top">Email:</td>
+									<td class="rowDataView"><b>xcreaw1@abv.bg</b></td>
+								</tr>
+																	<tr>
+										<td class="rowTitleView" valign="top">Телефон:</td>
+										<td class="rowDataView">0876237725</td>
+									</tr>
+																									<tr>
+										<td class="rowTitleView" valign="top">Адрес за кореспонденция:</td>
+										<td class="rowDataViewNormal">Дружба 2 блок 321</td>
+									</tr>
+															</table>
+						</td>
+					</tr>
+				</table>
+			</td>
+	</table>
+	<table cellspacing={0} cellpadding={0} border={0} class="cvTable" align="center">
+		<tr>
+			<td colspan=2>
+			<div style="padding-bottom:5px;font-weight:bold;font-size:15px;color:#000000;padding-top:20px;">Лична информация</div>
+			<table cellspacing=0 cellpadding=0 border=0  width=100% style="border-top:1px solid #000000;">
+				<tr>
+					<td class="rowTitleView" valign="top" style="width:100px;">Име:</td>
+					<td class="rowDataView">Емил</td>
+				</tr>
+								<tr>
+					<td class="rowTitleView" valign="top">Презиме:</td>
+					<td class="rowDataView">Руменов</td>
+				</tr>
+								<tr>
+					<td class="rowTitleView" valign="top">Фамилия:</td>
+					<td class="rowDataView">Златинов</td>
+				</tr>
+                                    <tr>
+                        <td class="rowTitleView" valign="top" style="padding-top:10px;">Пол:</td>
+                        <td  class="rowDataView" style="padding-top:10px;">Мъж</td>
+                    </tr>
+                                                    <tr>
+                        <td class="rowTitleView" valign="top">Дата на раждане:</td>
+                        <td  class="rowDataView">23.01.2002</td>
+                    </tr>
+                                                    <tr>
+                        <td class="rowTitleView" valign="top">Гражданство:</td>
+                        <td class="rowDataView">България</td>
+                    </tr>
+                                                    <tr>
+                        <td class="rowTitleView" valign="top" style="padding-top:10px;">В момента живея в:</td>
+                        <td class="rowDataViewNormal" style="padding-top:10px;">
+                                                            <b>София</b>/България                                                    </td>
+                    </tr>
+                			</table>
+					<div style="padding-bottom:5px;padding-top:20px;font-weight:bold;font-size:15px;color:#000000;">Образование</div>
+		<table cellspacing=0 cellpadding=0 border=0 style="border-top:1px solid #000000;" width={100%}>
+				  <tr>
+		  	<td valign=top>
+				<table cellspacing=0 cellpadding=0 border=0>
+					<tr>
+						<td class="rowTitleView" valign="top">Период:</td>
+						<td class="rowDataView" valign="top">Септември 2009 - Август 2021</td>
+					</tr>
+										<tr>
+						<td class="rowTitleView" valign="top">Специалност:</td>
+						<td class="rowDataView">Информационни технологии</td>
+					</tr>
+										<tr>
+						<td class="rowTitleView" valign="top">Степен/Ниво:</td>
+						<td class="rowDataViewNormal">Средно Училище</td>
+					</tr>
+					<tr>
+						<td class="rowTitleView" valign="top">Учебно заведение:</td>
+						<td class="rowDataView">105-то СУ Атанас Далчев</td>
+					</tr>
+					<tr>
+						<td class="rowTitleView" valign="top">Населено място:</td>
+						<td class="rowDataViewNormal">СОФИЯ</td>
+					</tr>
+										<tr>
+						<td class="rowTitleView" valign="top">Описание/основни предмети:</td>
+						<td class="rowDataViewNormal" valign="top">Програмиране и работа в ит сферата( Професионалният опит го имам от вкъщи)</td>
+					</tr>
+									 </table>
+				</td>
+			</tr>
+						</table>
+				<br><br><div style="padding-bottom:5px;font-weight:bold;font-size:15px;color:#000000;">Езици</div>
+		<table cellspacing=0 cellpadding=0 border=0 style="border-top:1px solid #000000;" width=100%>
+			<tr>
+				<td valign=top>
+					<table cellspacing=0 cellpadding=0 border=0>
+											<tr>
+							<td class="rowTitleView" valign="top" style="padding-bottom:10px;">Майчин Език:</td>
+							<td class="rowDataView" colspan=3  style="padding-bottom:10px;">Български</td>
+						</tr>
+																<tr>
+							<td class="rowDataView" valign="top" width=100>&nbsp;</td>
+							<td class="rowDataView" valign="top" width=100><b>Разбиране</b></td>
+							<td class="rowDataView" valign="top" width=100><b>Говорене</b></td>
+							<td class="rowDataView" valign="top" width=400><b>Писане</b></td>
+						</tr>
+												<tr>
+							<td valign="top" align=right style="padding-right:10px;padding-top:4px;"><b>Английски</b></td>
+							<td class="rowDataView" valign="top"><span class="spanDataView">Средно</span></td>
+							<td class="rowDataView" valign="top"><span class="spanDataView">Средно</span></td>
+							<td class="rowDataView" valign="top"><span class="spanDataView">Средно</span></td>
+						</tr>
+																<tr>
+							<td class="rowTitleView" valign="top" style="padding-top:10px;">Допълнителна информация:</td>
+							<td class="rowDataViewNormal" colspan=3 valign="top" style="padding-top:10px;">Github: https://github.com/StR0vErBG<br />
+								Лично портфолио: https://strover-portfolio.netlify.app/</td>
+						</tr>
+										</table>
+			</td>
+		</tr>
+	</table>
+				<br><br><div style="padding-bottom:5px;font-weight:bold;font-size:15px;color:#000000;">Умения</div>
+		<table cellspacing=0 cellpadding=0 border=0 style="border-top:1px solid #000000;" width=100%>
+		<tr>
+		  	<td valign=top>
+				<table cellspacing=0 cellpadding=0 border=0>
+									<tr>
+						<td class="rowTitleView" valign="top">Компютърни умения:</td>
+						<td class="rowDataView">Добри умения с технологиите HTML, CSS, SCSS, JavaScript, JQUERY, Bootstrap, NodeJS, Express, MongoDB, React, GIT.</td>
+					</tr>
+													<tr>
+						<td class="rowTitleView" valign="top">Умения:</td>
+						<td class="rowDataView">Бързо писане на клавиатура. Умея да седя дълго време пред компютъра и да се справям с всякакви проблеми. Уча бързо. Правя лесно организации. Работя лесно хора(разбира се има изключения).  </td>
+					</tr>
+												<tr>
+					<td class="rowTitleView" valign="top">Управлявал хора:</td>
+					<td class="rowDataView">Да (10)</td>
+				</tr>
+												</table>
+			</td>
+		</tr>
+		</table>
+				<br><br><div style="padding-bottom:5px;font-weight:bold;font-size:15px;color:#000000;">Допълнителни курсове и Сертификати</div>
+		<table cellspacing=0 cellpadding=0 border=0 style="border-top:1px solid #000000;" width=100%>
+			<tr>
+				<td class="rowTitleView" valign="top">Допълнителни курсове:</td>
+				<td class="rowDataViewNormal">В момента съм на web module в софтуни</td>
+			</tr>
+		</table>
+				</td>
+	</tr>
+		<tr>
+		<td colspan=2 align=right style="padding-top:20px;"><span style="color:#802990;font-size:15px;font-weight:bold;"><b>jobs.bg</b></span></td>
+	</tr>
+	</table>
+	
+	</div>
+  )
+}
