2e9b754daeffb325b7054fe31d85347ca8ab6851
StR0vErBG <xcreaw1@abv.bg>
2022-05-05 13:58:41 +0200

    Removing unnecesery things and removing nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Nav from "./components/nav/Navigation"
 import Homepage from "./components/Homepage/Homepage"
 import Cv from "./components/resume/Cv"
-import Footer from "./components/footer/Footer"
 
 import "./index.css"
 import { useEffect } from "react"
 
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -27,15 +25,13 @@ function App() {
   return (
     
     <>
-      <Nav/>
-    <Router>
-    
-    <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/cv" element={<Cv />}/>
-     
-        </Routes>
-        </Router>
+              <Router>
+                <Routes>
+                      <Route path="/" element={<Homepage />} />
+                      <Route path="/cv" element={<Cv />}/>
+              
+                </Routes>
+              </Router>
         <ToastContainer />
     </>
   );
