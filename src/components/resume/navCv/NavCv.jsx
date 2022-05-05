e8d49024b97eed0d25d4cdd61b744e07f28589e2
StR0vErBG <xcreaw1@abv.bg>
2022-05-05 14:05:42 +0200

    Making navCv

---
+++ src/components/resume/navCv/NavCv.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {AiOutlineArrowLeft} from "react-icons/ai"
+
+const Navcv = () => {
+    return (
+        <div>
+		<AiOutlineArrowLeft />
+            
+        </div>
+    );
+}
+
+export default Navcv;
