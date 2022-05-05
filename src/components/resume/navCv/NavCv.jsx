eed2641c42540777829db4d30b65d05c31ce29a7
StR0vErBG <xcreaw1@abv.bg>
2022-05-05 14:09:48 +0200

    Adding navigation to home

--- src/components/resume/navCv/NavCv.jsx
+++ src/components/resume/navCv/NavCv.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {AiOutlineArrowLeft} from "react-icons/ai"
+import "./navCv.scss"
+import {Link} from "react-router-dom"
 
 const Navcv = () => {
     return (
-        <div>
-		<AiOutlineArrowLeft />
+        <div className="navCv container-fluid mt-1">
+            <Link to="/">
+		            <AiOutlineArrowLeft className="icon"/>
+            </Link>
             
         </div>
     );
