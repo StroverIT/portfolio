168207edab8677bde6191991770022bf5a093ba2
StR0vErBG <xcreaw1@abv.bg>
2022-05-05 13:59:03 +0200

    Preparing for cv

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,16 +1,21 @@
+import Navigation from "./nav/Navigation"
 import React from 'react';
 import Header from './header/Header';
 import About from './about/About';
-import Contact from './contact/contact';
+import Contact from './contact/Contact';
+import Projects from "./projects/Projects"
 
 const Homepage = () => {
     return (
+        <>  
+        <Navigation />
         <div className="container">
               <Header />
       <About />
       <Projects />
       <Contact />
         </div>
+        </>
     );
 }
 
