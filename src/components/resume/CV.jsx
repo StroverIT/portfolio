d55c778276d364111844ab0e4c353370db63b62c
StR0vErBG <xcreaw1@abv.bg>
2022-05-05 14:05:36 +0200

    Making basic architecture

--- src/components/resume/CV.jsx
+++ src/components/resume/CV.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+
+// Components
+import Navcv from './navCv/NavCv';
+// Assets
 import Me from "../../assets/betterMe.jpg";
+// Styles
 import "./cv.scss"
 
 const Cv = () =>{
 	return(
 		<>
+		<Navcv />
 		<div className="container">
 			Dadsas
 			</div>
-			</>
+		</>
 	)
 }
 
