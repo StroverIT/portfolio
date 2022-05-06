233ceb8ed39ccbc2a4e1e7caf67adeb99eeb2fd6
StR0vErBG <xcreaw1@abv.bg>
2022-05-06 13:40:56 +0200

    Fixing bugs

--- src/components/resume/navCv/NavCv.jsx
+++ src/components/resume/navCv/NavCv.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {AiOutlineArrowLeft} from "react-icons/ai"
 import "./navCv.scss"
 import {Link} from "react-router-dom"
 
 const Navcv = () => {
+    if(document.body.classList.contains("blur")){
+        document.body.classList.remove("blur")
+    }
+    useEffect(()=>{
+          const item = document.querySelector(".navCv")
+          console.log(item);
+          if(item){
+              item.scrollIntoView({behavior: "smooth", inline:"start"});
+          }
+      }, [window.onload])
+
     return (
         <div className="navCv container-fluid mt-1">
             <Link to="/">
