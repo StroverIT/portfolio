b921c2615e451f4c3a55ce712490acc8acfa17ce
StR0vErBG <xcreaw1@abv.bg>
2022-05-13 20:28:36 +0200

    Fixing spacing and chaning to EN

--- src/components/resume/CV.jsx
+++ src/components/resume/CV.jsx
@@ -17,13 +17,13 @@ const Cv = () =>{
 					<img src={Me} alt="It's me" />
 					</div>
 					<section>
-						<div className="container-sm-fluid">
+						<div className="mx-sm-2">
 						<p className="mx-2 text-center text-sm-left myName">Емил Руменов Златинов</p>
 					<ul >
 						<li>Email: <b>emilzlatinov1@abv.bg</b> </li>
 
-						<li>Телефон: <b>0876237725</b></li>
-						<li>Адрес за кореспонденция: <b>Дружба 2 блок 321</b></li>
+						<li>Phone: <b>0876237725</b></li>
+						<li>Post address: <b>Drubja 2 block 321</b></li>
 					</ul>
 						</div>
 				
@@ -31,38 +31,62 @@ const Cv = () =>{
 
 			</section>
 			<section>
-				<h4>Лична информация</h4>
+				<h4>Personal Details</h4>
 				<hr />
 				<ul>
-					<li>Име: <b>Емил</b></li>
-					<li>Презиме: <b>Руменов</b></li>
-					<li>Фамилия: <b>Златинов</b></li>
-					<li>Гражданство: <b>Българско</b></li>
-					<li>В момента живея в: <b>София/България</b></li>
+					<li>First name: <b>Emil</b></li>
+					<li>Middle name: <b>Rumenov</b></li>
+					<li>Last name: <b>Zlatinov</b></li>
+					<li>Citizenship:: <b>Bulgarian</b></li>
+					<li>I live in:	: <b>Sofia/Bulgaria</b></li>
 				</ul>
 			</section>
 			<section>
-				<h4>Образование</h4>
+				<h4>Work Experience</h4>
 				<hr />
 				<ul>
-					<li>Период: <b>Септември 2009 - Август 2021</b></li>
-					<li>Специалност: <b>Информационни технологии</b></li>
-					<li>Степен/Ниво: <b>Средно Училище</b></li>
-					<li>Учебно заведение: <b>105-то СУ Атанас Далчев</b></li>
-					<li>Населено място: СОФИЯ</li>
-					<li>Описание/основни предмети: <b>Програмиране, английски език и графичен дизайн</b></li>
+					<li>Period: <b>December 2020 - January 2022</b></li>
+					<li>Job Title: <b>Freelancer</b></li>
+					<li>Company/Organization: <b>For me</b></li>
+					<li>Business sector:  <b>Information technologies</b></li>
+					<li>Location:  <b>Sofia</b></li>
+					<li>Additional Details:	  
+						<b>
+						<br />
+						Currently working sites that are made by me:
+						<br />
+						https://softoffice.bg
+						<br />
+						This portfolio
+						</b></li>
 				</ul>
 			</section>
 			<section>
-				<h4>Умения</h4>
+				<h4>Education</h4>
 				<hr />
 				<ul>
-					<li>Компютърни умения:	<b>Добри умения с технологиите HTML, CSS, SCSS, JavaScript, JQUERY, Bootstrap, NodeJS, Express, MongoDB, React, GIT.</b></li>
-					<li>Умения:	<b>Бързо писане на клавиатура. Умея да седя дълго време пред компютъра и да се справям с всякакви проблеми. Уча бързо. Правя лесно организации. Работя лесно с хора(разбира се има изключения).</b></li>
+					<li>Period: <b>September 2009 - August 2021</b></li>
+					<li>Major: <b>Information Technology</b></li>
+					<li>Level: <b>Secondary school</b></li>
+					<li>School: <b>Information Technologies, 105th Sofia University Atanas Dalchev</b></li>
+					<li>Location: Sofia</li>
+					<li>Description/Main Subjects: <b>Programming, English and graphic design</b></li>
 				</ul>
 			</section>
 			<section>
-			Допълнителни курсове и Сертификати: <b>В момента съм на web module в SoftUni. Като всички предишни модули съм ги минал с пълен и отличен 6</b>
+				<h4>Skills</h4>
+				<hr />
+				<ul>
+					<li>Computer Skills:	<b>HTML, CSS, SCSS, JavaScript, Jquery, Bootstrap, NodeJs, Express, MongoDB, React, Git. And now learning tailwind css</b></li>
+					<li>Personal skills:	<b>Problem solving. Fast writing(on keyboard), good communication skills. I can sit in front of the computer for a long time. And I can learn fast</b></li>
+				</ul>
+			</section>
+			<section>
+			<h4>Additional Courses and Certificates</h4>
+			<hr />
+			 <ul>
+				 <li>Additional Courses: <b>Currently on JS Web module in Softuni. I have passed all previous modules with a full 6</b></li>
+				 </ul>
 			</section>
 		</div>
 		</>
